e46c658601d5c4c98cdb3a144cc500c0ecca79d3
Aditya Avhad <aditya.avhad0707@gmail.com>
2026-01-22 14:56:01 +0530

    Create structure.c

---
+++ structure.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// Define a structure
+struct Student {
+    int roll;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    struct Student s1;
+
+    // Input values
+    printf("Enter Roll Number: ");
+    scanf("%d", &s1.roll);
+
+    printf("Enter Name: ");
+    scanf("%s", s1.name);
+
+    printf("Enter Marks: ");
+    scanf("%f", &s1.marks);
+
+    // Output values
+    printf("\n--- Student Details ---\n");
+    printf("Roll Number: %d\n", s1.roll);
+    printf("Name: %s\n", s1.name);
+    printf("Marks: %.2f\n", s1.marks);
+
+    return 0;
+}
